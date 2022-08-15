0f443feaa3dfafcdf7e0e47aa692f1277024630c
looloolalaa <dlrlgus65@nate.com>
2022-08-15 16:01:39 +0900

    basic functions done

--- MyMemo/ContentView.swift
+++ MyMemo/ContentView.swift
@@ -14,63 +14,80 @@ struct ContentView: View {
     @State private var newFileName = ""
     @State private var showingAlreadyExist = false
     
+    let layout = [
+        GridItem(.flexible(maximum: 100)),
+        GridItem(.flexible(maximum: 100)),
+        GridItem(.flexible(maximum: 100))
+    ]
+    
     var body: some View {
         NavigationView {
-            VStack {
-                ForEach(memos.items, id: \.title) { item in
-                    NavigationLink(destination: SecondView(memos: memos, text: item.content, item: item)){
-                        MemoIcon(memo: item)
+            VStack(alignment: .trailing) {
+                HStack {
+                    if showingFileNameField {
+                        HStack {
+                            TextField("name", text: $newFileName)
+                                .padding(7)
+                                .border(.gray, width: 1)
+                            
+                            
+                            Button("OK") {
+                                if !newFileName.isEmpty {
+                                    if memos.add(fileName: newFileName) {
+                                        newFileName = ""
+                                    } else {
+                                        showingAlreadyExist.toggle()
+                                    }
+                                }
+                            }
+                            .frame(height: 40)
+                            
+                        }
+                        .padding(.leading, 80)
+                        .alert(isPresented: $showingAlreadyExist) {
+                            Alert(title: Text("Already exists"))
+                        }
                     }
-                    .isDetailLink(false)
-                    //Prevent pop back
-                    //because pushed onto the navigation stack
+                    
+                    
+                    Spacer()
+                    
+                    //plus & minus button
+                    Button(action: {
+                        showingFileNameField.toggle()
+                    }){
+                        if showingFileNameField {
+                            Image(systemName: "minus.circle.fill")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.red)
+                        } else {
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                        }
+                    }
+                    .padding(.horizontal, 30)
+                    
                 }
                 
-                if showingFileNameField {
-                    HStack {
-                        TextField("name", text: $newFileName)
-                            .padding(4)
-                            .frame(width: 80)
-                            .border(.gray, width: 1)
-                        
-                        Button("OK") {
-                            if !newFileName.isEmpty {
-                                if memos.add(fileName: newFileName) {
-                                    newFileName = ""
-                                } else {
-                                    showingAlreadyExist.toggle()
-                                }
+                Divider()
+                    .padding(10)
+                
+                ScrollView {
+                    LazyVGrid(columns: layout, spacing: 20) {
+                        ForEach(memos.items, id: \.title) { item in
+                            NavigationLink(destination: SecondView(memos: memos, text: item.content, item: item)){
+                                MemoIcon(memo: item)
                             }
                         }
                     }
-                    .alert(isPresented: $showingAlreadyExist) {
-                        Alert(title: Text("Already exists"))
-                    }
                 }
                 
-                //plus & minus button
-                Button(action: {
-                    showingFileNameField.toggle()
-                }){
-                    if showingFileNameField {
-                        Image(systemName: "minus.circle.fill")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .padding()
-                            .foregroundColor(.red)
-                    } else {
-                        Image(systemName: "plus.circle.fill")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .padding()
-                    }
-                }
                 
             }
             .navigationTitle("Memo")
-            
         }
-        .navigationViewStyle(.stack)
     }
 }
 
